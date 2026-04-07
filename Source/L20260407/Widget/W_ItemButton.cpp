d075130a3d70578f491f1ecba425a33c7dd66794
BATMINJI <minjiconan@naver.com>
2026-04-07 22:42:23 +0900

    W_ItemButton 클래스 추가

--- Source/L20260407/Widget/W_ItemButton.cpp
+++ Source/L20260407/Widget/W_ItemButton.cpp
@@ -2,7 +2,62 @@
 
 
 #include "W_ItemButton.h"
+#include "Components/Button.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+#include "Components/Border.h"
+
+void UW_ItemButton::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (BTN_Item)
+	{
+		BTN_Item->OnClicked.AddDynamic(this, &UW_ItemButton::OnItemButtonClicked);
+	}
+}
 
 void UW_ItemButton::UpdateItemWidget(FName InRowName)
 {
+	RowName = InRowName;
+
+    if (!DT_Item) return;
+
+    FS_Item* ItemData = DT_Item->FindRow<FS_Item>(RowName, TEXT("FindRow"));
+
+    if (ItemData)
+    {
+        if (TXT_ItemName)
+        {
+            TXT_ItemName->SetText(ItemData->Name);
+        }
+        if (TXT_Desc)
+        {
+            TXT_Desc->SetText(ItemData->Desc);
+        }
+
+        if (TXT_Tier)
+        {
+            FText TierText = UEnum::GetDisplayValueAsText(ItemData->Tier);
+            TXT_Tier->SetText(TierText);
+        }
+
+        if (IMG_Icon)
+        {
+            IMG_Icon->SetBrushFromTexture(ItemData->Image);
+        }
+
+        if (Tier_BG)
+        {
+            Tier_BG->SetContentColorAndOpacity(FLinearColor(ItemData->TierBGColor));
+        }
+    }
+}
+
+void UW_ItemButton::OnItemButtonClicked()
+{
+    if (OnClickedItem.IsBound())
+    {
+        OnClickedItem.Broadcast(RowName);
+    }
 }
--- Source/L20260407/Widget/W_ItemButton.h
+++ Source/L20260407/Widget/W_ItemButton.h
@@ -4,21 +4,49 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
-#include "Data/S_Item"
+#include "L20260407/Data/S_Item.h"
 #include "W_ItemButton.generated.h"
 
-/**
- * 
- */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnClickedItemSignature, FName, RowName);
+
 UCLASS()
 class L20260407_API UW_ItemButton : public UUserWidget
 {
 	GENERATED_BODY()
 	
 public:
-	UPROPERTY(BlueprintCallable)
+    virtual void NativeConstruct() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UDataTable* DT_Item;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FName RowName;
+
+	UPROPERTY(BlueprintAssignable)
+	FOnClickedItemSignature OnClickedItem;
+
+	UFUNCTION(BlueprintCallable)
 	void UpdateItemWidget(FName InRowName);
 
-protected:
+    UFUNCTION()
+    void OnItemButtonClicked();
 
+    UPROPERTY(meta = (BindWidget))
+    class UButton* BTN_Item;
+
+    UPROPERTY(meta = (BindWidget))
+    class UImage* IMG_Icon;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* TXT_ItemName;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* TXT_Tier;
+
+    UPROPERTY(meta = (BindWidget))
+    class UBorder* Tier_BG;
+
+    UPROPERTY(meta = (BindWidget))
+    class UTextBlock* TXT_Desc;
 };
